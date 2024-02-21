d886e56f60bd6a66100b3debf643e900e59acffe
react <saiswarya155@gmail.com>
2024-02-21 15:19:48 +0530

    updated

---
+++ product.js
@@ -0,0 +1,28 @@
+var sidenav=document.getElementById("sidenav")
+var menuicon=document.getElementById("menuicon")
+var closenav=document.getElementById("closenav")
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+closenav.addEventListener("click",function(){
+sidenav.style.right="-50%"
+})
+
+//Autocomplete Product Search
+
+var search = document.getElementById('search');
+var product_container = document.getElementById('product-container');
+var product_list = product_container.querySelectorAll('div');
+console.log(product_list);
+
+search.addEventListener("keyup",function(){
+    var entered_name = this.value.toUpperCase();
+    for(count=0;count<product_list.length;count++){
+        var product_name = product_list[count].querySelector('h1').textContent.toUpperCase();
+        if(product_name.indexOf(entered_name) == -1){
+            product_list[count].style.display = "none";
+        }else{
+            product_list[count].style.display = "block";
+        }
+    }
+});
